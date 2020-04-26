b56df5e46311ffd986b5801e886258ed021619ff
Nidhi Agarwal <nidhi.agrl12@gmail.com>
2020-04-26 00:25:43 -0700

    sorting

--- Practice/src/Sorting.java
+++ Practice/src/Sorting.java
@@ -1,7 +1,12 @@
 
 public class Sorting {
 	
-	public static void MergeSort(int[] array) {
+	/*public static void MergeSortNikhilHelper(int[] array, int[] temp, int p, int q) {
+		
+		if arrat
+		int r = (p+q)/2;
+		
+		
 		int i,j,k;
 		int l = (array.length%2==0) ? (array.length/2)	: (array.length/2+1);
 		int[] array1 = new int[array.length/2];
@@ -39,6 +44,38 @@ public class Sorting {
 		}
 		
 			
+	}*/
+	
+	public static void MergeSort(int[] array) {
+
+		if(array.length>1) return;
+			
+		int[] array1 = new int[array.length/2];
+		int[] array2 = new int[array.length - array.length/2];
+		
+		//divide content of array into two arrays
+		for (int i=0;i<array.length/2; i++)
+			array1[i]=array[i];
+
+		for (int i=array.length/2; i<array.length;i++)
+			array2[i-array.length/2]=array[i];
+		
+		//send both arrays for sorting
+		MergeSort(array1);
+		MergeSort(array2);
+		
+		
+		int i=0, j=0, k=0; 
+		while (i<array1.length && j <array2.length)
+			if (array1[i] >array2[j])
+				array[k++]=array2[j++];
+			else 
+				array[k++]=array1[i++];
+
+		
+		while(i<array1.length) array[k++]=array1[i++];
+
+		while(j<array2.length) array[k++]=array2[j++];
 	}
 	
 	public static void main(String[] args) {
