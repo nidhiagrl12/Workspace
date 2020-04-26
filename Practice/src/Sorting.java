623278d37dd06e6676efac10150413d47e04e807
Nidhi Agarwal <nidhi.agrl12@gmail.com>
2020-04-26 00:07:22 -0700

    sorting

--- Practice/src/Sorting.java
+++ Practice/src/Sorting.java
@@ -1,176 +1,56 @@
 
 public class Sorting {
 	
-	public static void InsertionSort(int[] array) {
+	public static void MergeSort(int[] array) {
+		int i,j,k;
+		int l = (array.length%2==0) ? (array.length/2)	: (array.length/2+1);
+		int[] array1 = new int[array.length/2];
+		int[] array2 = new int[l];
 		
-		int y;
-		
-		for (int j=array.length;j>=0;j--)
-		
-		for (int i = 0; i <j-1; i++) {
-			if (array[i] > array[i + 1]) {
-				y = array[i + 1];
-				array[i + 1] = array[i];
-				array[i] = y;
-
-			}
+		//divide content of array into two arrays
+		for (i=0;i<array.length/2; i++) {
+			array1[i]=array[i];
 		}
-
-		for (int i = 0; i < array.length; i++) {
-			System.out.println(array[i]);
-		}
-	}
-	
-	public static void FacebookOn2(int[] a) {
-		int b[] = new int[a.length];
-			
-		for (int i=0;i<a.length;i++ ) {
-			b[i]=1;
-			for (int j=0;j<a.length; j++) {
-				if(i==j) continue;
-				b[i]=b[i]*a[j];
-			}
+		for (i=array.length/2; i<array.length;i++) {
+			array2[i-array.length/2]=array[i];
 		}
 		
+		//send both arrays for sorting
+		if(array1.length>1) MergeSort(array1);
+		if(array2.length>1) MergeSort(array2);
 		
-
-		for (int i = 0; i < a.length; i++) {
-			System.out.println(b[i]);
-		}
-	}
-	
-	public static int[] FacebookOn(int[] a) {
-
-		int b[] = new int[a.length];
 		
-		int zeroctr = 0, zeroLoc = 0, value = 1;
-
-		for (int i=0;i<a.length;i++ )
+		i=0; j=0; k=0; 
+		while (i<array1.length && j <array2.length)
 		{
-			b[i]=0;
-			if(a[i]==0)  
-			{
-				zeroctr++;
-				zeroLoc = i;
+			if (array1[i] >array2[j]) {
+				array[k]=array2[j]; j++; k++;
 			}
-			else
-				value *= a[i];
+			else { 
+				array[k]=array1[i]; i++; k++;
+			}
+		}
+		
+		while(i<array1.length) {
+			array[k]=array1[i]; i++; k++;
+		}
+		while(j<array2.length) {
+			array[k]=array2[j]; j++; k++;
+		}
+		
 			
-		}
-		
-		if(zeroctr > 1) return b;
-		
-		if(zeroctr == 1)
-		{
-			b[zeroLoc] = value;
-			return b;
-
-		}
-		
-		for (int i=0; i<=a.length-1; i++)
-			b[i] = (int) (value/a[i]);
-
-		
-		return b;
 	}
-	
-	
-	public static void MergeHelper(int[] array, int start, int end, int[] temp)
-	{
-		
-		if ((end - start ) < 2 ) // Stopping condition
-		{
-			if (((end-start) == 1) && (array[end] > array[start]))
-			{
-				int tmp = array[end];
-				array[end] = array[start];
-				array[start] = tmp;
-			}
-		}
-		else
-		{
-			int mid = start + (end-start)/2;
-			MergeHelper(array, start, mid, temp);
-			MergeHelper(array, mid+1, end, temp);
-			
-			int i = start;
-			int j = mid+1;
-			int k = 0;
-			
-			while ((i<=mid) && (j<=end))
-				if (array[i]>array[j])
-					temp[k++] = array[j++];
-				else
-					temp[k++] = array[i++];
-			
-			while (i<=mid)
-				temp[k++] = array[i++];
-
-			while (j<=end)
-				temp[k++] = array[j++];
-
-			for(int kk=0; k<kk; k++)
-				array[start+kk] = temp[kk];
-		}
-	}
-	
-	public static int[] Merge(int[] array)
-	{
-		int[] temp = new int[array.length];
-		MergeHelper(array, 0, array.length-1, temp);
-		return array;
-	}
-	
-	
-
-	/*public static int[] Merge(int[] array, int p, int q, int r) {
-		
-		int array_c[] = new int[r+1];
-		int Len_c = r+1;
-		int Len_a = q-p+1;
-		int Len_b = r-q;
-		int k = 0, j=q+1,i=p;
-		
-		while(k<Len_c) {
-			if(Len_a >= i+1 && Len_b >=j+1) {
-				if(array[i] > array[j]) {
-					array_c[k]=array[j];j++;
-				}
-				else {
-					array_c[k]=array[i];i++;
-				}
-			}
-			else {
-				if(Len_a <i) {
-					array_c[k]=array[j];j++;
-				}
-				else {
-					array_c[k]=array[i];i++;
-				}
-			}
-			k++;		
-		}
-		return array_c;
-	}*/
-	
 	
 	public static void main(String[] args) {
-		// int[] array = { 100, 543, 3, 200, 2, 1, 8, 11, 45, 11, 66,3,7,100,34,44 };
-		int[] array2= {3,2, 5,3,4,4};
-
-		/* InsertionSort(array);*/
-		/*FacebookOn2(array2);*/
-		/*int[] answer = FacebookOn(array2);*/
+		int[] array = { 3, 200, 2, 5,1,34,53,11,0,2};
+		//InsertionSort(array);
 		
-		// int p= 0, q=(array2.length)/2-1, r=array2.length-1;
+		// Merge(array);
+		MergeSort(array);
+		for (int z = 0; z < array.length; z++)
+			System.out.println(array[z]);
+			System.out.println("------");
 		
-		// int[] answer = Merge(array2,p,q,r);
-		
-		
-		int[] answer = Merge(array2);
-		
-		for (int i = 0; i < answer.length; i++)
-			System.out.println(answer[i]);
 
 	}
 
